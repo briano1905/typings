e2097e707302c1352e5eed215b34d715488bc1ac
Jack <jack@inter-concept.co.uk>
2019-07-27 00:27:46 +0100

    - added a 'themes' window that shows all themes and allows to quickly change between them (new themes MUST be added to themes/theme-list.json to be visible in this window) you can show it by clicking 'themes' in the footer - changed the footer to allow for multiple elements - fixed body background getting broken when the content is taller than the view height

--- main.js
+++ main.js
@@ -365,3 +365,52 @@ function getCookie(cname) {
   }
   return '';
 }
+
+showAllThemes();
+function showAllThemes(){
+    fetch(`themes/theme-list.json`)
+    .then(response => {
+      if (response.status === 200) {
+        response
+          .text()
+          .then(body => {
+            let themes = JSON.parse(body);
+            let keys = Object.keys(themes);
+            for(let i = 0;i < keys.length; i ++){
+
+              let theme = document.createElement('div');
+              theme.setAttribute('class', 'theme-button');
+              theme.setAttribute('onClick', `setTheme('${keys[i]}')`);
+              if(themes[keys[i]]['customHTML'] != undefined){
+                theme.style.background = themes[keys[i]]['background'];
+                theme.innerHTML = themes[keys[i]]['customHTML']
+              }else{
+                theme.textContent = keys[i];
+                theme.style.background = themes[keys[i]]['background'];
+                theme.style.color = themes[keys[i]]['color'];
+              }
+              document.getElementById('theme-area').appendChild(theme);
+
+            }
+          })
+          .catch(err => console.error(err));
+      } else {
+        console.log(`Cant find theme-list.json`);
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function showThemeCenter() {
+  console.log('test');
+  document.getElementById('theme-center').classList.remove('hidden');
+  document.getElementById('command-center').classList.add('hidden');
+}
+
+function hideThemeCenter() {
+  console.log('test');
+  document.getElementById('theme-center').classList.add('hidden');
+  document.getElementById('command-center').classList.remove('hidden');
+}
+
+
