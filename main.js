2f2faa88b0662621fdb3f5762394876922bfc233
jakergrossman <jake.r.grossman@gmail.com>
2019-08-02 17:55:41 -0500

    Add punctuation to inputFieldClass

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ inputField.addEventListener('keydown', e => {
       if (timerActive) inputFieldClass();
   }
   function inputFieldClass() {
-    if (e.key >= 'a' && e.key <= 'z') {
+    if (e.key >= 'a' && e.key <= 'z' || (e.key === `'` || e.key === ',' || e.key === '.' || e.key === ';')) {
       let inputWordSlice = inputField.value + e.key;
       let currentWordSlice = wordList[currentWord].slice(0, inputWordSlice.length);
       inputField.className = inputWordSlice === currentWordSlice ? '' : 'wrong';
